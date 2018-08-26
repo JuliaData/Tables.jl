3dfae2e9c137671e4969b3d9499fa8b3e69fea48
quinnj <quinn.jacobd@gmail.com>
2018-08-26 00:06:56 -0600

    Cleanup enumerable support

--- src/enumerable.jl
+++ src/enumerable.jl
@@ -1,57 +1,38 @@
 using .QueryOperators
 
+nondatavaluetype(::Type{DataValue{T}}) where {T} = Union{T, Missing}
+nondatavaluetype(::Type{T}) where {T} = T
+
+Base.@pure function nondatavaluetype(::Type{NT}) where {NT <: NamedTuple{names}} where {names}
+    TT = Tuple{Any[ nondatavaluetype(fieldtype(NT, i)) for i = 1:fieldcount(NT) ]...}
+    return NamedTuple{names, TT}
+end
+
+struct DataValueUnwrapRow{T}
+    row::T
+end
+
+unwrap(x) = x
+unwrap(x::DataValue) = isna(x) ? missing : DataValues.unsafe_get(x)
+Base.getproperty(d::DataValueUnwrapRow, ::Type{T}, col::Int, nm::Symbol) where {T} = unwrap(getproperty(getfield(d, 1), T, col, nm))
+Base.getproperty(d::DataValueUnwrapRow, nm::Symbol) = unwrap(getproperty(getfield(d, 1), nm))
+Base.propertynames(d::DataValueUnwrapRow) = propertynames(getfield(d, 1))
+
 struct DataValueUnwrapper{NT, S}
     x::S
 end
 
-Base.eltype(rows::DataValueUnwrapper{NT, S}) where {NT, S} = NT
-Base.IteratorSize(::Type{<:DataValueUnwrapper}) = Base.SizeUnknown()
+Base.eltype(rows::DataValueUnwrapper) = DataValueUnwrapRow{eltype(rows.x)}
+Base.IteratorSize(::Type{DataValueUnwrapper{NT, S}}) where {NT, S} = Base.IteratorSize(S)
 
-function Tables.schema(e::QueryOperators.Enumerable)
-    sch = eltype(e)
-    return NamedTuple{names(sch), Tuple{map(nondatavaluetype, types(sch))...}}
-end
+Tables.schema(e::QueryOperators.Enumerable) = nondatavaluetype(eltype(e))
 Tables.rows(e::E) where {E <: QueryOperators.Enumerable} = DataValueUnwrapper{schema(e), E}(e)
 
-unwrap(x) = x
-unwrap(x::DataValue) = isna(x) ? missing : DataValues.unsafe_get(x)
-
-function Base.iterate(rows::DataValueUnwrapper{NamedTuple{names, types}, S}) where {names, types, S}
-    if @generated
-        vals = Tuple(:(unwrap(getproperty(row, $(Meta.QuoteNode(nm))))) for (nm, T) in zip(names, types.parameters))
-        q = quote
-            x = iterate(rows.x)
-            x === nothing && return nothing
-            row, st = x
-            return ($(NamedTuple{names, types})(($(vals...),)), st)
-        end
-        # @show q
-        return q
-    else
-        x = iterate(rows.x)
-        x === nothing && return nothing
-        row, st = x
-        return NamedTuple{names, types}(Tuple(unwrap(getproperty(row, nm)) for (nm, T) in zip(names, types.parameters))), st
-    end
-end
-
-function Base.iterate(rows::DataValueUnwrapper{NamedTuple{names, types}, S}, st) where {names, types, S}
-    if @generated
-        vals = Tuple(:(unwrap(getproperty(row, $(Meta.QuoteNode(nm))))) for (nm, T) in zip(names, types.parameters))
-        q = quote
-            x = iterate(rows.x, st)
-            x === nothing && return nothing
-            row, st = x
-            return ($(NamedTuple{names, types})(($(vals...),)), st)
-        end
-        # @show q
-        return q
-    else
-        x = iterate(rows.x, st)
-        x === nothing && return nothing
-        row, st = x
-        return NamedTuple{names, types}(Tuple(unwrap(getproperty(row, nm)) for (nm, T) in zip(names, types.parameters))), st
-    end
+function Base.iterate(rows::DataValueUnwrapper{NT}, st=()) where {NT <: NamedTuple{names}} where {names}
+    x = iterate(rows.x, st...)
+    x === nothing && return nothing
+    row, st = x
+    return DataValueUnwrapRow(row), (st,)
 end
 
 using IteratorInterfaceExtensions
