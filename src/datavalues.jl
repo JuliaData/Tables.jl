6e2b162a682dc41b57d2a297986eb22f586e20a2
quinnj <quinn.jacobd@gmail.com>
2018-09-10 23:20:12 -0600

    Cleanup integration w/ queryverse; provide Tables.DataValueUnwrapper for sinks to unwrap potentially DataValue-based NamedTuples

--- src/datavalues.jl
+++ src/datavalues.jl
@@ -1,23 +1,10 @@
 using .DataValues
 
-# DataValue-compatible row iteration for Data.Sources
+# DataValue overloads for query.jl definitions
 nondatavaluetype(::Type{DataValue{T}}) where {T} = Union{T, Missing}
-nondatavaluetype(::Type{T}) where {T} = T
-Base.@pure function nondatavaluetype(::Type{NT}) where {NT <: NamedTuple{names}} where {names}
-    TT = Tuple{Any[ nondatavaluetype(fieldtype(NT, i)) for i = 1:fieldcount(NT) ]...}
-    return NamedTuple{names, TT}
-end
-
-unwrap(x) = x
 unwrap(x::DataValue) = isna(x) ? missing : DataValues.unsafe_get(x)
-
 datavaluetype(::Type{T}) where {T <: DataValue} = T
-datavaluetype(::Type{T}) where {T} = T
 datavaluetype(::Type{Union{T, Missing}}) where {T} = DataValue{T}
-Base.@pure function datavaluetype(::Tables.Schema{names, types}) where {names, types}
-    TT = Tuple{Any[ datavaluetype(fieldtype(types, i)) for i = 1:fieldcount(types) ]...}
-    return NamedTuple{names, TT}
-end
 
 struct DataValueRowIterator{NT, S}
     x::S
@@ -26,6 +13,7 @@ DataValueRowIterator(::Type{NT}, x::S) where {NT <: NamedTuple, S} = DataValueRo
 
 "Returns a DataValue-based NamedTuple-iterator"
 DataValueRowIterator(::Type{Schema{names, types}}, x::S) where {names, types, S} = DataValueRowIterator{datavaluetype(NamedTuple{names, types}), S}(x)
+
 function datavaluerows(x)
     r = Tables.rows(x)
     #TODO: add support for unknown schema
@@ -54,6 +42,9 @@ function Base.iterate(rows::DataValueRowIterator{NT, S}, st=()) where {NT <: Nam
         return NT(Tuple(fieldtype(NT, i)(getproperty(row, nondatavaluetype(fieldtype(NT, i)), i, names[i])) for i = 1:fieldcount(NT))), (st,)
     end
 end
+
+# Alternative lazy row implementation; currently though, Query.jl relies on
+# being able to infer return types via all the type information of NamedTuples
 
 # function Base.iterate(rows::DataValueRowIterator{NT}, st=()) where {NT}
 #     state = iterate(rows.x, st...)
