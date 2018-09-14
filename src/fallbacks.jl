e69685d3e8e17fe60f36f17617ff40829ed75c4e
quinnj <quinn.jacobd@gmail.com>
2018-09-14 11:33:46 -0600

    Support duplicate column names (kinda) in generic columns fallback

--- src/fallbacks.jl
+++ src/fallbacks.jl
@@ -39,6 +39,29 @@ end
 # build columns from rows
 haslength(L) = L isa Union{Base.HasShape, Base.HasLength}
 
+struct IntermediateTable{names, T}
+    values::T
+end
+IntermediateTable{names}(values::T) where {names, T} = IntermediateTable{names, T}(values)
+Base.propertynames(it::IntermediateTable{names, T}) where {names, T} = names
+Base.getproperty(it::IntermediateTable{names, T}, nm::Symbol) where {names, T} = getfield(it, 1)[columnindex(names, nm)]
+
+function merge(it::IntermediateTable{names, types}, j::Int, v::T) where {names, types, T}
+    vals = getfield(it, 1)
+    return IntermediateTable{names}(Tuple(ifelse(i == j, v, vals[i]) for i = 1:length(names)))
+end
+
+import Base: ==
+==(a::IntermediateTable{n}, b::IntermediateTable{n}) where {n} = getfield(a, 1) == getfield(b, 1)
+==(a::IntermediateTable, b::IntermediateTable) = false
+==(a::IntermediateTable{n}, b::NamedTuple{n}) where {n} = getfield(a, 1) == Tuple(b)
+==(a::IntermediateTable, b::NamedTuple) = false
+
+Base.isequal(a::IntermediateTable{n}, b::IntermediateTable{n}) where {n} = isequal(getfield(a, 1), getfield(b, 1))
+Base.isequal(a::IntermediateTable, b::IntermediateTable) = false
+Base.isequal(a::IntermediateTable{n}, b::NamedTuple{n}) where {n} = isequal(getfield(a, 1), Tuple(b))
+Base.isequal(a::IntermediateTable, b::NamedTuple) = false
+
 """
     Tables.allocatecolumn(::Type{T}, len) => returns a column type (usually AbstractVector) w/ size to hold `len` elements
     
@@ -49,15 +72,15 @@ allocatecolumn(T, len) = Vector{T}(undef, len)
 @inline function allocatecolumns(::Schema{names, types}, len) where {names, types}
     if @generated
         vals = Tuple(:(allocatecolumn($(fieldtype(types, i)), len)) for i = 1:fieldcount(types))
-        return :(NamedTuple{names}(($(vals...),)))
+        return :(IntermediateTable{names}(($(vals...),)))
     else
-        return NamedTuple{names}(Tuple(allocatecolumn(fieldtype(types, i), len) for i = 1:fieldcount(types)))
+        return IntermediateTable{names}(Tuple(allocatecolumn(fieldtype(types, i), len) for i = 1:fieldcount(types)))
     end
 end
 
 # add! will push! or setindex! a value depending on if the row-iterator HasLength or not
-@inline add!(val, col::Int, nm::Symbol, ::Union{Base.HasLength, Base.HasShape{1}}, nt, row) = setindex!(nt[col], val, row)
-@inline add!(val, col::Int, nm::Symbol, T, nt, row) = push!(nt[col], val)
+@inline add!(val, col::Int, nm::Symbol, ::Union{Base.HasLength, Base.HasShape{1}}, nt, row) = setindex!(getfield(nt, 1)[col], val, row)
+@inline add!(val, col::Int, nm::Symbol, T, nt, row) = push!(getfield(nt, 1)[col], val)
 
 @inline function buildcolumns(schema, rowitr::T) where {T}
     L = Base.IteratorSize(T)
@@ -72,7 +95,7 @@ end
 @inline add!(dest::AbstractVector, val, ::Union{Base.HasLength, Base.HasShape{1}}, row) = setindex!(dest, val, row)
 @inline add!(dest::AbstractVector, val, T, row) = push!(dest, val)
 
-@inline function add_or_widen!(dest::AbstractVector{T}, val::S, nm::Symbol, L, row, len, updated) where {T, S}
+@inline function add_or_widen!(dest::AbstractVector{T}, val::S, nm::Symbol, L, col, row, len, updated) where {T, S}
     if S === T || val isa T
         add!(dest, val, L, row)
         return dest
@@ -80,26 +103,26 @@ end
         new = allocatecolumn(Base.promote_typejoin(T, S), max(len, length(dest)))
         row > 1 && copyto!(new, 1, dest, 1, row - 1)
         add!(new, val, L, row)
-        updated[] = merge(updated[], NamedTuple{(nm,)}((new,)))
+        updated[] = merge(updated[], col, new)
         return new
     end
 end
 
 @inline function add_or_widen!(val, col, nm, L, columns, row, len, updated)
-    @inbounds add_or_widen!(columns[col], val, nm, L, row, len, updated)
+    @inbounds add_or_widen!(getfield(columns, 1)[col], val, nm, L, col, row, len, updated)
     return
 end
 
 # when Tables.schema(x) === nothing
 function buildcolumns(::Nothing, rowitr::T) where {T}
     state = iterate(rowitr)
-    state === nothing && return NamedTuple()
+    state === nothing && return IntermediateTable{(), Tuple{}}()
     row, st = state
     names = propertynames(row)
     L = Base.IteratorSize(T)
     len = haslength(L) ? length(rowitr) : 0
     sch = Schema(names, nothing)
-    columns = NamedTuple{names}(Tuple(Union{}[] for _ = 1:length(names)))
+    columns = IntermediateTable{names}(Tuple(Union{}[] for _ = 1:length(names)))
     return _buildcolumns(rowitr, row, st, sch, L, columns, 1, len, Ref{Any}(columns))
 end
 
