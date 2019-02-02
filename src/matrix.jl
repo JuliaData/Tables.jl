36289c8676f4b5f8782967d961e99e289c092099
Jacob Quinn <quinn.jacobd@gmail.com>
2019-02-02 00:32:01 -0700

    Cleanups

--- src/matrix.jl
+++ src/matrix.jl
@@ -1,12 +1,12 @@
-istable(::Type{<:Matrix}) = false
+istable(::Type{<:AbstractMatrix}) = false
 
-rows(m::T) where {T <: Matrix} = throw(ArgumentError("a '$T' is not a table; see `?Tables.astable` for ways to treat a Matrix as a table"))
-columns(m::T) where {T <: Matrix} = throw(ArgumentError("a '$T' is not a table; see `?Tables.astable` for ways to treat a Matrix as a table"))
+rows(m::T) where {T <: AbstractMatrix} = throw(ArgumentError("a '$T' is not a table; see `?Tables.table` for ways to treat an AbstractMatrix as a table"))
+columns(m::T) where {T <: AbstractMatrix} = throw(ArgumentError("a '$T' is not a table; see `?Tables.table` for ways to treat an AbstractMatrix as a table"))
 
 struct MatrixTable{T}
     names::Vector{Symbol}
     lookup::Dict{Symbol, Int}
-    matrix::Union{Matrix{T}, Adjoint{T, Matrix{T}}}
+    matrix::T
 end
 
 istable(::Type{<:MatrixTable}) = true
@@ -18,14 +18,14 @@ struct MatrixRow{T}
     source::MatrixTable{T}
 end
 
-Base.getproperty(m::MatrixRow, ::Type{T}, col::Int, nm::Symbol) where {T} =
+Base.getproperty(m::MatrixRow, ::Type, col::Int, nm::Symbol) =
     getfield(getfield(m, 2), 3)[getfield(m, 1), col]
 Base.getproperty(m::MatrixRow, nm::Symbol) =
     getfield(getfield(m, 2), 3)[getfield(m, 1), getfield(getfield(m, 2), 2)[nm]]
 Base.propertynames(m::MatrixRow) = names(getfield(m, 2))
 
 rowaccess(::Type{<:MatrixTable}) = true
-schema(m::MatrixTable{T}) where {T} = Schema(Tuple(names(m)), NTuple{size(getfield(m, 3), 2), T})
+schema(m::MatrixTable{T}) where {T} = Schema(Tuple(names(m)), NTuple{size(getfield(m, 3), 2), eltype(T)})
 rows(m::MatrixTable) = m
 Base.eltype(m::MatrixTable{T}) where {T} = MatrixRow{T}
 Base.length(m::MatrixTable) = size(getfield(m, 3), 1)
@@ -42,13 +42,23 @@ Base.getproperty(m::MatrixTable, ::Type{T}, col::Int, nm::Symbol) where {T} = ge
 Base.getproperty(m::MatrixTable, nm::Symbol) = getfield(m, 3)[:, getfield(m, 2)[nm]]
 Base.propertynames(m::MatrixTable) = names(m)
 
-function astable(m::Union{Adjoint, Matrix}; header::Vector{Symbol}=[Symbol("Column$i") for i = 1:size(m, 2)])
+"""
+Tables.table(m::AbstractMatrix; header::Vector{Symbol}=...)
+
+Convert an AbstractMatrix (Matrix, Adjoint, etc.) to a MatrixTable, which satisfies the Tables.jl interface. This allows accesing the matrix via `Tables.rows` and `Tables.columns`. An optional keyword argument `header` can be passed as a Vector{Symbol} to be used as the column names. Note that no copy of the AbstractMatrix is made.
+"""
+function table(m::AbstractMatrix; header::Vector{Symbol}=[Symbol("Column$i") for i = 1:size(m, 2)])
     length(header) == size(m, 2) || throw(ArgumentError("provided column names `header` length must match number of columns in matrix ($(size(m, 2))"))
     lookup = Dict(nm=>i for (i, nm) in enumerate(header))
     return MatrixTable(header, lookup, m)
 end
 
-function asmatrix(table)
+"""
+Tables.matrix(table)
+
+Convert any table source input to a Matrix. If the table column types are not homogenous, they will be promoted to the most common type in the materialized Matrix. Note that column names are ignored in the conversion.
+"""
+function matrix(table)
     cols = Tables.columns(table)
     types = schema(cols).types
     T = reduce(promote_type, types)
