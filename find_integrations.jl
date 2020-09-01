b68d77ff73e12cfc5cab44272caa8b72a74147b2
Tamas K. Papp <tkpapp@gmail.com>
2020-09-01 11:12:27 +0200

    Make find_integrations.jl a bit more general, update integrations.
    1. Paths no longer hardcoded to @quinnj's computer.
    2. Can be started from any directory.
    3. Sort package names alphabetically.
    
    Also update the list itself.

--- find_integrations.jl
+++ find_integrations.jl
@@ -1,18 +1,33 @@
-# Run in .julia/registries directory
+####
+#### automatically generate a list of integrations
+####
+
+###
+### Usage
+###
+### 1. ensure a development version of Tables.jl (`pkg> dev Tables`) in `DEPOT_PATH[1]/dev/Tables`
+### 2. make sure the General registry is up to date (`pkg> up`)
+### 3. run this script, which uses the first depot from DEPOT_PATH
+
+DEPOT = first(DEPOT_PATH)
+REGISTRIES = joinpath(DEPOT, "registries")
 
 # find each package w/ a direct dependency on Tables.jl
-pkgs = dirname.(readlines(`grep -rl ^Tables General/. --include=Deps.toml`))
+pkgs = cd(REGISTRIES) do
+    dirname.(readlines(`grep -rl ^Tables General/. --include=Deps.toml`))
+end
 
-pkgnames = [splitpath(x)[end] for x in pkgs]
+pkgnames = sort([splitpath(x)[end] for x in pkgs])
 
 function parseurl(file)
     repo = readlines(file)[end]
     return strip(split(repo, " = ")[end], '"')
 end
 
-urls = [parseurl(joinpath("General", string(first(nm)), nm, "Package.toml")) for nm in pkgnames]
+urls = [parseurl(joinpath(REGISTRIES, "General", string(first(nm)), nm, "Package.toml"))
+        for nm in pkgnames]
 
-open("/Users/jacobquinn/.julia/dev/Tables/INTEGRATIONS.md", "w+") do io
+open(joinpath(DEPOT, "dev", "Tables", "INTEGRATIONS.md"), "w+") do io
     println(io, "Packages currently integrating with Tables.jl:")
     for (nm, url) in zip(pkgnames, urls)
         println(io, "* [$nm]($url)")
