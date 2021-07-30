1b714297fb1b49e0e1e56762c19d7104d50f0bc8
Steven G. Johnson <stevenj@alum.mit.edu>
2021-07-30 14:09:28 -0400

    more docs

--- src/matrix.jl
+++ src/matrix.jl
@@ -45,8 +45,8 @@ columnnames(m::MatrixTable) = names(m)
 """
     Tables.table(m::AbstractVecOrMat; [header])
 
-Wrap an `AbstractVecOrMat` (`Matrix`, `Adjoint`, etc.) in a `MatrixTable`, which satisfies the
-Tables.jl interface. This allows accessing the matrix via `Tables.rows` and `Tables.columns`.
+Wrap an `AbstractVecOrMat` (`Matrix`, `Vector`, `Adjoint`, etc.) in a `MatrixTable`, which satisfies the
+Tables.jl interface.  (An `AbstractVector` is treated as a 1-column matrix.) This allows accessing the matrix via `Tables.rows` and `Tables.columns`.
 An optional keyword argument iterator `header` can be passed which will be converted to a
 `Vector{Symbol}` to be used as the column names. Note that no copy of the `AbstractVecOrMat`
 is made.
