ca505ee6e12e4f61c7fd6a3946168d70d4dee171
Bogumił Kamiński <bkamins@sgh.waw.pl>
2022-08-16 14:54:20 +0200

    Remove istable for a value of type AbstractMatrix (#198)
    I think - as in other places - we should only define a method for a type, and use a default fallback for the value.
    
    @quinnj Was there some specific reason why in this case a method for a value was introduced?

--- src/matrix.jl
+++ src/matrix.jl
@@ -1,4 +1,3 @@
-istable(::AbstractMatrix) = false
 istable(::Type{<:AbstractMatrix}) = false
 
 rows(m::T) where {T <: AbstractMatrix} = throw(ArgumentError("a '$T' is not a table; see `?Tables.table` for ways to treat an AbstractVecOrMat as a table"))
