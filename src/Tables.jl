22e74b7e56d035bc16f9bdc1f774d5cd8b0f2ac7
Anthony Blaom, PhD <anthony.blaom@gmail.com>
2022-10-11 02:51:45 +1300

    fix docstring for Tables.rows (#307)

--- src/Tables.jl
+++ src/Tables.jl
@@ -414,8 +414,8 @@ the columns of the input.
 The [`Tables.Schema`](@ref) of an `AbstractRow` iterator can be queried via `Tables.schema(rows)`,
 which may return `nothing` if the schema is unknown.
 Column names can always be queried by calling `Tables.columnnames(row)` on an individual row,
-and row values can be accessed by calling `Tables.getcolumn(rows, i::Int )` or
-`Tables.getcolumn(rows, nm::Symbol)` with a column index or name, respectively.
+and row values can be accessed by calling `Tables.getcolumn(row, i::Int )` or
+`Tables.getcolumn(row, nm::Symbol)` with a column index or name, respectively.
 
 See also [`rowtable`](@ref) and [`namedtupleiterator`](@ref).
 """
